a137a4067c294b73ff7f41d18c61fd92877cb580
Martin Holters <martin.holters@hsu-hh.de>
2025-03-19 08:49:11 +0100

    Add license information

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
+import fs from 'node:fs/promises';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import license from 'rollup-plugin-license';
 
+const licensecontent = await fs.readFile(new URL('LICENSE.md', import.meta.url), {
+  encoding: 'utf-8',
+});
 export default defineConfig({
   plugins: [sveltekit()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        license({
+          sourcemap: true,
+
+          banner: {
+            commentStyle: 'ignored',
+            content: `
+              ${licensecontent}
+
+              This bundle contains code from various dependencies licensed as follows:
+
+              <% _.forEach(dependencies, function (dependency) { %>
+              = <%= dependency.name %> v<%= dependency.version %>
+
+              <%= dependency.licenseText %>
+              <% }) %>
+            `,
+          },
+        }),
+      ],
+    },
+  },
 });
