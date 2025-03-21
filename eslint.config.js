70118295c49e204aceeac4b545c45e0dfc9f9613
Martin Holters <martin.holters@hsu-hh.de>
2025-03-21 13:17:44 +0100

    Set up ESlint

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import { fileURLToPath } from 'node:url';
+
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import svelte from 'eslint-plugin-svelte';
+
+import svelteConfig from './svelte.config.js';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  },
+);
